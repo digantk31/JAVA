8245c4b7e5f903dc014ccd6f53477f488fcd4198
Digant kathiriya <105538783+digantk31@users.noreply.github.com>
2024-08-01 00:57:31 +0530

    Update keybRead.java

--- keybRead.java
+++ keybRead.java
@@ -6,7 +6,7 @@ public class keybRead {
     public static void main(String[] args) {
         // create object
         Scanner s = new Scanner(System.in);
-    //Scanner(class_name) s(reference) = new Scanner(Constructor) (System.in)(is associated with a keyboard)
+    //Scanner(class_name) s(reference) = new Scanner(Constructor) (System.in)(built-in object (is associated with a keyboard))
         
         int a,b,c;
         System.out.println("Enter 2 Num");
