486e58f24114169f89aea989f3d914a3b2fae226
Digant kathiriya <105538783+digantk31@users.noreply.github.com>
2024-07-31 22:54:01 +0530

    Update MyFirst.java

--- MyFirst.java
+++ MyFirst.java
@@ -8,3 +8,22 @@ class MyFirst{
 }
 
 // Hello World
+
+
+
+/* 
+But when we write "public" before class MyFirst, then it is mandatory to 
+FILE NAME = CLASS NAME
+EX. MyFirst.java = class MyFirst
+*/
+
+/* Example:
+import java.lang.*;
+
+public class MyFirst{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");
+    }
+}
+*/
