9ef9b4635cef934e62037cd5edb021bb8d8f2e32
Digant kathiriya <105538783+digantk31@users.noreply.github.com>
2024-07-31 23:46:20 +0530

    Update ScannerMethods.java

--- ScannerMethods.java
+++ ScannerMethods.java
@@ -1,1 +1,20 @@
+/*
+Class Scanner
 
+  nextInt() ----> return integar
+  nextFloat() ----> return float
+  nextDouble() ---->return double
+
+  next() ----> read string (name or characters) -- just one Word
+  nextLine() ----> read line -- multiple words
+
+  nextByte() ---->
+  nextShort() ---->
+  nextLong() ---->
+  nextBoolean() ---->
+
+return boolean values
+  either TRUE or FALSE
+  hasNextInt() 
+  hasNextFloat()
+*/  
